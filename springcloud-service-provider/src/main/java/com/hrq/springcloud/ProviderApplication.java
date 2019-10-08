c2348fc445f2176cf0f074608dd25481f03b8cdd
何润强 <1584204039@qq.com>
2019-10-08 16:51:02 +0800

    开启redis缓存

--- springcloud-service-provider/src/main/java/com/hrq/springcloud/ProviderApplication.java
+++ springcloud-service-provider/src/main/java/com/hrq/springcloud/ProviderApplication.java
@@ -4,6 +4,7 @@ import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.hrq.springcloud")
 @MapperScan(value = {"classpath:mapperconfig/*.xml","com.hrq.springcloud.mapper"})
 @EnableDistributedTransaction//开启分布式事务
+@EnableCaching//开启缓存
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
